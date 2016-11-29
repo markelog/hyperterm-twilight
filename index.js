1e0e5f81cf2eeacd953952a267a704cc7b269f81
Tye Trask <inc3pt@gmail.com>
2016-11-29 09:40:53 -0800

    Sets border color to cursor color
    Sets border color to cursor color (as darkGray const) so that when
    using split pane mode, pane borders (and other stuff) are visible
    
    Closes #1

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const yellow = '#9b703f';
 const blue = '#7587a6';
 const magenta = '#9b859d';
 const cyan = '#cda869';
-const gray = '#9d9d9d'
+const gray = '#9d9d9d';
+const darkGray = '#3c4043';
 const white = '#f8f8f8';
 
 const foreground = white;
 const background = black;
 
-const border = black;
-const cursor = '#3c4043';
+const border = darkGray;
+const cursor = darkGray;
 
 exports.decorateConfig = config => {
     return Object.assign({}, config, {
